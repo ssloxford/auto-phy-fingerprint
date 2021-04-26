73166c5ffadde9e657fd7897ad15b44c61706370
RB <bulk@richard-baker.net>
2021-04-26 21:01:16 +0100

    Chmod docker scripts

--- demod/adsb-libmodes-demod/run.sh
+++ demod/adsb-libmodes-demod/run.sh
@@ -1,1 +1,1 @@
-docker run -dit --rm -v /home/sdr/dockerplayground:/radio --name autophyfp-demod-adsb autophyfp-demod-adsb
+docker run -dit --rm -v /home/sdr/dockerplayground:/radio --name autophyfp-demod-adsb autophyfp-demod-adsb /radio/$1
